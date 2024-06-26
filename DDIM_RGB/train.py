35b3ea4eb7c40e8034ab1ae0041e83208558eb85
yuanyuanyo <yuanyuann.hu@gmail.com>
2024-06-26 10:36:44 +0200

    Add files via upload

---
+++ DDIM_RGB/train.py
@@ -0,0 +1,49 @@
+from dataset import create_dataset
+from model.UNet import UNet
+from utils.engine import GaussianDiffusionTrainer
+from utils.tools import train_one_epoch, load_yaml, plot_losses
+import torch
+from utils.callbacks import ModelCheckpoint
+
+
+def train(config):
+    consume = config["consume"]
+    if consume:
+        cp = torch.load(config["consume_path"])
+        config = cp["config"]
+    print(config)
+
+    device = torch.device(config["device"])
+    loader = create_dataset(**config["Dataset"])
+    start_epoch = 1
+
+    model = UNet(**config["Model"]).to(device)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=config["lr"], weight_decay=1e-4)
+    trainer = GaussianDiffusionTrainer(model, **config["Trainer"]).to(device)
+
+    model_checkpoint = ModelCheckpoint(**config["Callback"])
+
+    if consume:
+        model.load_state_dict(cp["model"])
+        optimizer.load_state_dict(cp["optimizer"])
+        model_checkpoint.load_state_dict(cp["model_checkpoint"])
+        start_epoch = cp["start_epoch"] + 1
+
+    losses = []
+
+    for epoch in range(start_epoch, config["epochs"] + 1):
+        loss = train_one_epoch(trainer, loader, optimizer, device, epoch)
+        print(f'Epoch [{epoch}], Train Loss: {loss:.4f}')
+
+        losses.append(loss)
+
+        model_checkpoint.step(loss, model=model.state_dict(), config=config,
+                              optimizer=optimizer.state_dict(), start_epoch=epoch,
+                              model_checkpoint=model_checkpoint.state_dict())
+
+    plot_losses(losses, "./data/loss_plot.png")
+
+
+if __name__ == "__main__":
+    config = load_yaml("config.yml", encoding="utf-8")
+    train(config)
